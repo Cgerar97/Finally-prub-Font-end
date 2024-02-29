fc4f0082d655cc2de3385213d9d11301510ce220
Cgerardo9701~ <colmenaresg97@gmail.com>
2024-02-29 18:24:19 +0100

    finally-prub-front

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Navbar = () => {
+
+
+  return (
+
+
+    <div>
+        <ul>
+            
+<li>Soy el primero</li>
+              <li>Soy el Segundo</li>
+              <li>Soy el Tercero</li>
+<li>Soy el cuarto</li>
+<li>Soy el quinto</li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+        </ul>
+
+
+
+
+
+
+
+    </div>
+
+
+
+  )
+}
+
+export default Navbar
---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-red-500">
+<h1>
+  hola
+</h1>
+
+
+
+
+    </div>
+  );
+}
